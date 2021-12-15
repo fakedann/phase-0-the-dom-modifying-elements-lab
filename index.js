1622db79e0588de856022ab4edc3f7b1d830691f
Daniel Escalona <daniel07escalona@gmail.com>
2021-12-15 17:32:02 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const element = document.getElementById("main");
+element.remove();
+
+const newHeader = document.createElement("H1");
+newHeader.id = "victory";
+newHeader.innerHTML = "YOUR-NAME is the champion"
+document.body.append(newHeader);
